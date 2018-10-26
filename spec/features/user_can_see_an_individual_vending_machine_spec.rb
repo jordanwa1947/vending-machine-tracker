e31dd150ccb88a1d7c261f47bf60f38a53096564
Jordan Whitten <jordanwa1947@gmail.com>
2018-10-26 11:06:25 -0600

    added testing display of snacks

--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -4,9 +4,14 @@ feature 'When a user visits a vending machine show page' do
   scenario 'they see the location of that machine' do
     owner = Owner.create(name: "Sam's Snacks")
     dons  = owner.machines.create(location: "Don's Mixed Drinks")
+    snack_1 = dons.snacks.create(name: 'White Castle Burger', price: 3.50)
+    snack_1 = dons.snacks.create(name: 'Flaming Hot Cheetos', price: 2.50)
 
     visit machine_path(dons)
 
     expect(page).to have_content("Don's Mixed Drinks Vending Machine")
+    expect(page).to have_content("White Castle Burger: $3.50")
+    expect(page).to have_content("Flaming Hot Cheetos: $2.50")
+    expect(page).to have_content("Average Price: $2.50")
   end
 end
