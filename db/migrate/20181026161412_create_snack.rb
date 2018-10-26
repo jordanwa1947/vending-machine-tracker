d319b25214cd722d585426e8d6768b04b2b7c479
Jordan Whitten <jordanwa1947@gmail.com>
2018-10-26 10:55:37 -0600

    snack and machine_snacks tabels with relation tests

---
+++ db/migrate/20181026161412_create_snack.rb
@@ -0,0 +1,8 @@
+class CreateSnack < ActiveRecord::Migration[5.1]
+  def change
+    create_table :snacks do |t|
+      t.integer :price
+      t.string :name
+    end
+  end
+end
--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -4,5 +4,11 @@ describe Machine, type: :model do
   describe 'validations' do
     it { should validate_presence_of :location }
     it { should belong_to :owner }
+    it { should have_many :snacks}
+  end
+
+  describe "relationships" do
+    it { should have_many(:machine_snacks) }
+    it { should have_many(:snacks).through(:machine_snacks) }
   end
 end
